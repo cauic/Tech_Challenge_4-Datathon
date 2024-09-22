6fe4f6f89cde65f070d45fac71810785f605695e
cauic <166946440+cauic@users.noreply.github.com>
2024-09-22 14:10:55 -0300

    Update TC5-Datathon.py

--- TC5-Datathon.py
+++ TC5-Datathon.py
@@ -13,7 +13,7 @@ def render_tech_challenge_5():
     st.write("")
     st.markdown('### Objetivo') 
     st.markdown('Esse é um projeto da pós-graduação em Data Analytics pela FIAP.')
-    st.markdown('Nosso objetivo é criar uma solução preditiva que demonstre o impacto da ONG Passos Mágicos na comunidade que atende.')
+    st.markdown('Nosso objetivo é criar uma solução preditiva que ajude a ONG Passos Mágicos a ampliar o seu impacto social na comunidade que atende.')
     st.markdown('Essa ONG, que atua há 31 anos em um município carente de São Paulo, busca transformar vidas por meio da educação. O desafio inclui análise e visualização de dados, machine learning e o deploy em produção.')
     st.markdown('### Autores')
     st.markdown('Laio Soares')
